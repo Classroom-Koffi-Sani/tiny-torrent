5124911259ae156be66169e8eecd1846ef065e7b
blaesus <corec443@gmail.com>
2019-07-12 23:56:26 +0000

    implementation menu client

--- src/tg/defitech/tinytorrent/ClientImpl.java
+++ src/tg/defitech/tinytorrent/ClientImpl.java
@@ -5,10 +5,65 @@
  */
 package tg.defitech.tinytorrent;
 
+import java.io.File;
+import java.util.Scanner;
+import tg.defitech.tinytorrent.api.Client;
+
 /**
  *
  * @author blaesus
  */
-public class ClientImpl {
+public class ClientImpl implements Client{
     
+   
+    @Override
+    public File share(File file, String trackerURL) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public File download(TinyTorrent torrent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public File getSharedFolder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    public static void partagerRessource(){
+        System.out.println("---PARTAGE FICHIER---");
+    }
+    
+    public static void telechargerRessource(){
+        System.out.println("---TELECHARGEMENT FICHIER---");
+    }
+    
+    public static void menu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---MENU---");
+        int rep = 0;
+        while(rep == 0){
+             System.out.println("[1]->partager un fichier");
+             System.out.println("[2]->Telecharger un fichier");
+             rep = sc.nextInt();
+             if(rep == 1){
+                 partagerRessource();
+             }
+             else if(rep == 1){
+                 telechargerRessource();
+             }
+             else{
+                 rep = 0;
+             }
+        }
+       
+    }
+    
+    public static void main(String[] args){
+        //String sharedFolderLink = args[0];
+        menu();
+        
+    }
 }
--- src/tg/defitech/tinytorrent/TrackerImpl.java
+++ src/tg/defitech/tinytorrent/TrackerImpl.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package tg.defitech.tinytorrent;
-
+import java.rmi.RemoteException;
+import java.util.Set;
+import tg.defitech.tinytorrent.api.RemoteClient;
+import tg.defitech.tinytorrent.api.Tracker;
 /**
  *
  * @author blaesus
  */
-public class TrackerImpl {
+public class TrackerImpl implements Tracker{
+
+    @Override
+    public void register(HashValue resourceHashValue, RemoteClient client) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void unregister(HashValue resourceHashValue, RemoteClient client) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<HashValue> findResourceHashValues() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<HashValue> findResourceHashValues(RemoteClient client) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<RemoteClient> findClients(HashValue resourceHashValue) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
